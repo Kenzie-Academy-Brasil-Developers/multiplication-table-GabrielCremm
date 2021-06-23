623badda4c26a5c299f06f9471c2a27816e57004
GabrielCremm <gcscremm@hotmail.com>
2021-06-23 19:46:55 -0300

    Entrega tabela de Multiplicacao

---
+++ script.js
@@ -0,0 +1,12 @@
+function tabela(n){
+  let x =[];                           
+  for(let i =0; i <= n; i++){
+    x [i]=[];
+
+    for(let cont=0; cont <= n; cont++)
+      x[i][cont] = i*j
+      
+  }
+  console.table(x)
+} 
+  
