55815b584337d7cd50f018b96a7cf62821f048fa
GabrielCremm <gcscremm@hotmail.com>
2021-06-25 10:04:40 -0300

    arrumando codigo

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function tabela(n){
-  let x =[];                           
-  for(let i =0; i <= n; i++){
-    x [i]=[];
 
-    for(let cont=0; cont <= n; cont++)
-      x[i][cont] = i*j
-      
-  }
-  console.table(x)
-} 
-  
